f4fa163e5249a35726a6897cd90ab61e27520a9a
PauloFonseca <pfonseca.arq@gmail.com>
2023-11-23 08:32:14 -0300

    Adicione à página uma paleta contendo quatro cores distintas - Adicione a cor preta como a primeira cor da paleta de cores

---
+++ script.js
@@ -0,0 +1,15 @@
+const color1 = document.getElementsByClassName('color')[0];
+
+color1.style.backgroundColor = 'black';
+
+const color2 = document.getElementsByClassName('color')[1];
+
+color2.style.backgroundColor = 'blue';
+
+const color3 = document.getElementsByClassName('color')[2];
+
+color3.style.backgroundColor = 'yellow';
+
+const color4 = document.getElementsByClassName('color')[3];
+
+color4.style.backgroundColor = 'green';
