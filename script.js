a7fcb42529577e8e8584728fbd1b91e97d96a4e6
PauloFonseca <pfonseca.arq@gmail.com>
2023-11-23 08:36:21 -0300

    Adicione um botão para gerar cores aleatórias para a paleta de cores

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ color3.style.backgroundColor = 'yellow';
 const color4 = document.getElementsByClassName('color')[3];
 
 color4.style.backgroundColor = 'green';
+
+const btnGenerationColor = document.getElementById('button-random-color');
+
+function generateColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+btnGenerationColor.addEventListener('click', () => {
+  color2.style.backgroundColor = generateColor();
+  color3.style.backgroundColor = generateColor();
+  color4.style.backgroundColor = generateColor();
+});
